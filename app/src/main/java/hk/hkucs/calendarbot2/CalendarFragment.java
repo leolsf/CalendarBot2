ee5fda1920131d5c9e5f644c51ff22e05587c6c1
jacqueline-weng <jwengab@gmail.com>
2020-04-11 22:25:13 +0800

    Material Calendar View

--- app/src/main/java/hk/hkucs/calendarbot2/CalendarFragment.java
+++ app/src/main/java/hk/hkucs/calendarbot2/CalendarFragment.java
@@ -12,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class CalendarFragment extends Fragment {
     private static final String ARG_COUNT = "param1";
     private Integer counter;
@@ -46,7 +48,7 @@ public class CalendarFragment extends Fragment {
     @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //view.setBackgroundColor(ContextCompat.getColor(getContext(), COLOR_MAP[counter]));
-        CalendarView calendarView = view.findViewById(R.id.simpleCalendarView);
+        MaterialCalendarView calendarView = view.findViewById(R.id.simpleCalendarView);
 //        TextView textViewCounter = view.findViewById(R.id.calendar_counter);
 //        textViewCounter.setText("Calendar");
 //        if(counter == 0){
